c4d8d8b1ac895a8da07fc97c852e7ffad1092112
Marinos <76847464+marinVcq@users.noreply.github.com>
2022-07-12 20:33:03 +0200

    Add Theme file and color global const

---
+++ src/abstract/colors.js
@@ -0,0 +1,37 @@
+/* defining a global JS file for colors and use it wherevere its required */
+
+export const COLORS = {
+
+	veryDarkDesaturatedBlue: 'hsl(222, 26%, 31%)',
+	veryDarkDesaturatedBlue2: 'hsl(223, 31%, 20%)',
+	veryDarkDesaturatedBlue3: 'hsl(224, 36%, 15%)',
+	desaturatedDarkBlue: 'hsl(225, 21%, 49%)',
+	desaturatedDarkBlue2: 'hsl(224, 28%, 35%)',
+	red: 'hsl(6, 63%, 50%)',
+	darkRed: 'hsl(6, 70%, 34%)',
+	lightGrayishOrange: 'hsl(30, 25%, 89%)',
+	grayishOrange: 'hsl(28, 16%, 65%)',
+	veryDarkGrayishBlue: 'hsl(221, 14%, 31%)',
+
+	lightGray: 'hsl(0, 0%, 90%)',
+	grayishRed: 'hsl(0, 5%, 81%)',
+	veryLightGray: 'hsl(0, 0%, 93%)',
+	darkModerateCyan: 'hsl(185, 42%, 37%)',
+	veryDarkCyan: 'hsl(185, 58%, 25%)',
+	orange: 'hsl(25, 98%, 40%)',
+	darkOrange: 'hsl(25, 99%, 27%)',
+	lightGrayishYellow: 'hsl(45, 7%, 89%)',
+	darkGrayishOrange: 'hsl(35, 11%, 61%)',
+	veryDarkGrayishYellow: 'hsl(60, 10%, 19%)',
+
+	veryDarkViolet: 'hsl(268, 75%, 9%)',
+	veryDarkViolet2: 'hsl(268, 71%, 12%)',
+	darkViolet: 'hsl(281, 89%, 26%)',
+	vividMagenta: 'hsl(285, 91%, 52%)',
+	pureCyan: 'hsl(176, 100%, 44%)',
+	softCyan: 'hsl(177, 92%, 70%)',
+	veryDarkViolet3: 'hsl(268, 47%, 21%)',
+	darkMagenta: 'hsl(290, 70%, 36%)',
+	lightYellow: 'hsl(52, 100%, 62%)',
+	veryDarkBlue4: 'hsl(198, 20%, 13%)',
+}
---
+++ src/abstract/theme.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { COLORS } from "./colors.js";
+
+export const theme1 = {
+  mainBackground: COLORS.veryDarkDesaturatedBlue,
+  keypadBackground: COLORS.veryDarkDesaturatedBlue2,
+  screenBackground: COLORS.veryDarkDesaturatedBlue3,
+
+  resetBackground: COLORS.desaturatedDarkBlue,
+  resetShadow: COLORS.desaturatedDarkBlue2,
+
+  equalsBackground: COLORS.red,
+  equalsShadow: COLORS.darkRed,
+
+  deleteBackground: COLORS.desaturatedDarkBlue,
+  deleteShadow: COLORS.desaturatedDarkBlue2,
+
+  numbersBackground: COLORS.lightGrayishOrange,
+  numbersShadow: COLORS.grayishOrange,
+
+  toggle: COLORS.red,
+  toggleBackground: COLORS.veryDarkDesaturatedBlue2,
+
+  numbersText: COLORS.veryDarkGrayishBlue,
+  equalsText: "white",
+  resetText: "white",
+  deleteText: "white",
+  topText: "white", 
+  screenText: "white",
+
+}
+
+export const theme2 = {
+
+  mainBackground: COLORS.lightGray,
+  keypadBackground: COLORS.grayishRed,
+  screenBackground: COLORS.veryLightGray,
+
+  resetBackground: COLORS.darkModerateCyan,
+  resetShadow: COLORS.veryDarkCyan,
+
+  deleteBackground: COLORS.darkModerateCyan,
+  deleteShadow: COLORS.veryDarkCyan,
+
+  equalsBackground: COLORS.orange,
+  equalsShadow: COLORS.darkOrange,
+
+  numbersBackground: COLORS.lightGrayishYellow,
+  numbersShadow: COLORS.darkGrayishOrange,
+
+  toggle: COLORS.orange,
+  toggleBackground: COLORS.grayishRed,
+
+  numbersText: COLORS.veryDarkGrayishYellow,
+  equalsText: "white",
+  resetText: "white",
+  deleteText: "white",
+  topText: COLORS.veryDarkGrayishYellow, 
+  screenText: COLORS.veryDarkGrayishYellow,
+
+}
+
+export const theme3 = {
+
+  mainBackground: COLORS.veryDarkViolet,
+  keypadBackground: COLORS.veryDarkViolet2,
+  screenBackground: COLORS.veryDarkViolet2,
+
+  resetBackground: COLORS.darkViolet,
+  resetShadow: COLORS.vividMagenta,
+
+  deleteBackground: COLORS.darkViolet,
+  deleteShadow: COLORS.vividMagenta,
+
+  equalsBackground: COLORS.pureCyan,
+  equalsShadow: COLORS.softCyan,
+
+  numbersBackground: COLORS.veryDarkViolet3,
+  numbersShadow: COLORS.darkMagenta,
+
+  toggle: COLORS.pureCyan,
+  toggleBackground: COLORS.veryDarkViolet2,
+
+  numbersText: COLORS.lightYellow,
+  equalsText: COLORS.veryDarkBlue4,
+  resetText: "white",
+  deleteText: "white",
+  topText: COLORS.lightYellow, 
+  screenText: COLORS.lightYellow,
+}
