f1690019e29a4f61ec596fe3eccca0b4090a3af6
Marinos <76847464+marinVcq@users.noreply.github.com>
2022-07-13 19:36:36 +0200

    refactor with ternary condition

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Top from './components/Top';
 import Screen from './components/Screen';
 import Keypad from './components/Keypad';
 import Key from './components/Key';
+import Footer from './components/Footer'
 
 /* Themes and Style */
 import {theme1,theme2,theme3} from "./abstract/theme.js";
@@ -29,49 +30,54 @@ const App = () => {
     value: "0",
   });
 
+  /* Toggle theme function */
+  const switchTheme = (event) => {
+    event.preventDefault();
+    setTheme({
+      active: theme.active === theme1
+      ? theme2
+      : theme.active === theme2
+      ? theme3
+      : theme1,
+    });
+  }
+
   /* Delete function */
-  const deleteHandler = (e) => {
-    e.preventDefault();
+  const deleteHandler = (event) => {
+    event.preventDefault();
     /* Set value to "0" if delete all the expression */
-    if(calc.value.length === 1){
-      setCalc({
-        ...calc,
-        value: "0",
-      });     
-    }else{
-      setCalc({
-        ...calc,
-        value: calc.value.toString().slice(0,-1),
-      });      
-    }
+    setCalc({
+      value: calc.value.length === 1 ? "0" : calc.value.toString().slice(0,-1),
+    });
   }
 
   /* Reset function */
-  const resetHandler = (e) => {
-    e.preventDefault();
-
+  const resetHandler = (event) => {
+    event.preventDefault();
     setCalc({
-      ...calc,
       value: "0",
     });
   }
 
   /* Resolve expression function */
   const resolve = (expression) => {
+
+    /* Replace commas by decimal point */
+    expression = expression.replaceAll(',','.');
+
+    /* Handle errors */
     try{
-      return eval(expression);
+      return eval(expression).toFixed(3);
     }catch(error){
-      console.log('ERROR');
       return "0";
     }
   }
 
   /* Get result function */
-  const resultHandler = (e) => {
-    e.preventDefault();
+  const resultHandler = (event) => {
+    event.preventDefault();
     setCalc({
-      ...calc,
-      value: resolve(calc.value),
+      value: resolve(calc.value).replaceAll('.',','),
     });
   }
 
@@ -81,131 +87,77 @@ const App = () => {
     event.preventDefault();
     let operator = event.target.value;
 
-    if( operator === '-' && calc.value[calc.value.length - 1] === "-"){ /* Operator can't follow an other (except + or -) */
-      setCalc({
-        ...calc,
-        value: calc.value.slice(0,-1) + "+",
-      });
-    }else if(operator === '+' && calc.value[calc.value.length - 1] === "+"){
-       setCalc({
-        ...calc,
-        value: calc.value.slice(0,-1) + "+",
-      });     
-    }else if(operator === "/" && operators.includes(calc.value[calc.value.length -1])){
-        setCalc({
-          ...calc,
-          value: calc.value,
-        });
-    }else if(operator === "x"){
-
-      if(calc.value[calc.value.length -1] === "*"){
-        setCalc({
-          ...calc,
-          value: calc.value,
-        });         
-      }else{
-        setCalc({
-          ...calc,
-          value: calc.value + "*",
-        });         
-      }
-    
-    }else{
-      setCalc({
-        ...calc,
-        value: calc.value + operator,
-      });
-    }
+    setCalc({
+      value: ( operator === '-' && calc.value[calc.value.length - 1] === "-")
+        ? calc.value.slice(0,-1) + "+"
+        : (operator === '+' && calc.value[calc.value.length - 1] === "+")
+        ? calc.value.slice(0,-1) + "+"
+        : (operator === "/" && operators.includes(calc.value.replaceAll('*','x')[calc.value.length -1]))
+        ? calc.value
+        : (operator === "x" && calc.value[calc.value.length -1] === "*")
+        ? calc.value
+        : (operator === "x" && calc.value[calc.value.length -1] !== "*")
+        ? calc.value + "*"
+        : calc.value + operator
+    });
   }
 
   /* Number function*/
   const numberHandler = (event) => {
     event.preventDefault();
-
     let number = event.target.value;
-
-    if(calc.value === "0"){
-      setCalc({
-        ...calc,
-        value: number,
-      })     
-    }else{
-      setCalc({
-        ...calc,
-        value:  calc.value + number,
-      })      
-    }
+    /* Initial zero replace by first input number */
+    setCalc({
+      value: calc.value === "0"
+        ? number
+        : calc.value + number,
+    });
   }
 
   /* Comma function */
-  const commaHandler = (e) => {
-    e.preventDefault();
+  const commaHandler = (event) => {
+
+    event.preventDefault();
 
     /* Avoid succesion of commas */
-    if( calc.value[calc.value.length -1] === "."){
-      setCalc({
-        ...calc,
-        value: calc.value,
-      });
-    }else{
-      setCalc({
-        ...calc,
-        value: calc.value + ".",
-      });
-    }
-  }
-
-  /* Toggle theme function */
-  const switchTheme = () => {
-
-    if(theme.active === theme1){
-      setTheme({
-        active: theme2,
-      })
-    }else if(theme.active === theme2){
-      setTheme({
-        active: theme3,
-      })      
-    }else{
-      setTheme({
-        active: theme1,
-      })
-    }
+    setCalc({
+      value: calc.value[calc.value.length -1] === ","
+        ? calc.value
+        : calc.value + ",",
+    });
   }
 
   return (
-      <ThemeProvider theme={theme.active}>
-        <Wrapper className="notranslate" >
+    <ThemeProvider theme={theme.active}>
+      <Wrapper >
+          <Top onClick={switchTheme}/>
+          <Screen value={calc.value}/>
+          <Keypad>
+            {
+              btnValues.flat().map((btn, i) => {
+                return (
+                  <Key key={i} value={btn} className={btn === "=" ? "equals" : btn === "RESET" ? "reset" : btn === "DEL" ? "delete" : "number"}
 
-            <Top onClick={switchTheme}/>
-
-            <Screen value={calc.value}/>
-
-            <Keypad>
-              {
-                btnValues.flat().map((btn, i) => {
-                  return (
-                    <Key key={i} value={btn} className={btn === "=" ? "equals" : btn === "RESET" ? "reset" : btn === "DEL" ? "del" : ""}
-
-                    onClick={
-                      btn === "DEL"
-                      ? deleteHandler
-                      : btn === "RESET"
-                      ? resetHandler
-                      : btn === "="
-                      ? resultHandler
-                      : operators.includes(btn)
-                      ? operatorHandler
-                      : btn === "."
-                      ? commaHandler
-                      : numberHandler
-                    }
-                    ></Key>
-                  );
-                })}
-              </Keypad>
-        </Wrapper>
-      </ThemeProvider>
+                  onClick={
+                    btn === "DEL"
+                    ? deleteHandler
+                    : btn === "RESET"
+                    ? resetHandler
+                    : btn === "="
+                    ? resultHandler
+                    : operators.includes(btn)
+                    ? operatorHandler
+                    : btn === "."
+                    ? commaHandler
+                    : numberHandler
+                  }
+                  ></Key>
+                );
+              })}
+            </Keypad>
+      </Wrapper>
+      <Footer/>
+    </ThemeProvider>
   )
 };
 
